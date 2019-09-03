8173c8c5357ce6c3490f6440bc6fefe51b39152c
Nur Alfian Julianda <nalfianjuli@gmail.com>
2019-09-03 21:39:33 +0700

    dteail

---
+++ MyMovie/Controller/DetailMovieViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailMovieViewController.swift
+//  MyMovie
+//
+//  Created by Unknown on 03/09/19.
+//  Copyright © 2019 Nalfian. All rights reserved.
+//
+
+import UIKit
+
+class DetailMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
--- MyMovie/Controller/NowPlayingViewController.swift
+++ MyMovie/Controller/NowPlayingViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class NowPlayingViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
