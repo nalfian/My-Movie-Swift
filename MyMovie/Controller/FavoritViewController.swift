136c85389b8451f1ccbb3b17e7a22c469af18682
Nur Alfian Julianda <nalfianjuli@gmail.com>
2019-09-03 21:01:51 +0700

    tab bar contreller

---
+++ MyMovie/Controller/FavoritViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritViewController.swift
+//  MyMovie
+//
+//  Created by Unknown on 03/09/19.
+//  Copyright © 2019 Nalfian. All rights reserved.
+//
+
+import UIKit
+
+class FavoritViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ MyMovie/Controller/UpcomingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UpComingViewController.swift
+//  MyMovie
+//
+//  Created by Unknown on 03/09/19.
+//  Copyright © 2019 Nalfian. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UpcomingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+    
+}
